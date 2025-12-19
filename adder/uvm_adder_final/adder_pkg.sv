6f19c24ec9739eec576ee01a9d7eaf2f83a5272f
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-12-19 11:21:14 +0900

    Create adder_pkg.sv

---
+++ adder/uvm_adder_final/adder_pkg.sv
@@ -0,0 +1,16 @@
+package adder_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "adder_transfer.sv"
+  `include "adder_sequencer.sv"
+  `include "adder_driver.sv"
+  `include "adder_monitor.sv"
+  `include "adder_agent.sv"
+
+  `include "adder_seq_item.sv"
+  `include "adder_seq_lib.sv"
+  `include "adder_vseq_lib.sv"
+endpackage
+
+`include "adder_if.sv"
