c78f0133191b9c1c7aab1390778634b3148df06e
HOJUN_KIM <whwhwhwl93@gmail.com>
2025-12-19 11:20:56 +0900

    Add interface instantiation for UVM adder

---
+++ adder/uvm_adder_final/intf_insts.sv
@@ -0,0 +1,12 @@
+
+adder_if adder_intf(.i_clk(SystemClock), .i_rstn(nReset));
+
+assign dut.i_clk            = SystemClock           ;
+assign dut.i_rstn           = nReset                ;
+assign dut.i_enable         = adder_intf.i_enable   ;
+assign dut.i_a              = adder_intf.i_a        ;
+assign dut.i_b              = adder_intf.i_b        ;
+assign dut.i_cin            = adder_intf.i_cin      ;
+
+assign adder_intf.o_result  = dut.o_result          ;
+assign adder_intf.o_valid   = dut.o_valid           ;
